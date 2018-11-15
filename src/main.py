ebc5d307b2d57a83c7acf2c95bee0030d07e8d00
prodrigu <paroque28@gmail.com>
2018-11-14 20:30:58 -0600

    input vector done

--- src/main.py
+++ src/main.py
@@ -4,33 +4,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-def readWav(audio_filename, numcep):
+def input_vector(audio_filename, numcep, numcontext):
     # Load wav files
     fs, audio = wav.read(audio_filename)
     
     # Get mfcc coefficients
     orig_inputs = mfcc(audio, samplerate=fs, numcep=numcep)
     
-    ig, ax = plt.subplots()
-    mfcc_data= np.swapaxes(orig_inputs, 0 ,1)
-    cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.jet, origin='lower', aspect='auto')
-    ax.set_title('MFCC')
-    #Showing mfcc_data
-    plt.show()
-    #Showing orig_inputs
-    plt.plot(orig_inputs)
-    plt.show()
+    # We only keep every second feature (BiRNN stride = 2) ???
+    orig_inputs = orig_inputs[::2]
+
+    ###PLOT
+#     ig, ax = plt.subplots()
+#     mfcc_data= np.swapaxes(orig_inputs, 0 ,1)
+#     cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.jet, origin='lower', aspect='auto')
+#     ax.set_title('MFCC')
+#     #Showing mfcc_data
+#     plt.show()
+#     #Showing orig_inputs
+#     plt.plot(orig_inputs)
+#     plt.show()
+    ###END PLOT
+
+
+
     # For each time slice of the training set, we need to copy the context this makes
+    # the numcep dimensions vector into a numcep + 2*numcep*numcontext dimensions
+    # because of:
+    #  - numcep dimensions for the current mfcc feature set
+    #  - numcontext*numcep dimensions for each of the past and future (x2) mfcc feature set
+    # => so numcep + 2*numcontext*numcep
     train_inputs = np.array([], np.float32)
     train_inputs.resize((orig_inputs.shape[0], numcep + 2 * numcep * numcontext))
     
-    for time_slice in range(train_inputs.shape[0]):
-        # Pick up to numcontext time slices in the past,
-        # And complete with empty mfcc features
-        need_empty_past = max(0, ((time_slices[0] + numcontext) - time_slice))
+    # Prepare pre-fix post fix context
+    empty_mfcc = np.array([])
+    empty_mfcc.resize((numcep))
+    # Prepare train_inputs with past and future contexts
+    time_slices = range(train_inputs.shape[0])
+    context_past_min = time_slices[0] + numcontext
+    context_future_max = time_slices[-1] - numcontext
+    for time_slice in time_slices:
+        # Reminder: array[start:stop:step]
+        # slices from indice |start| up to |stop| (not included), every |step|
+
+        # Add empty context data of the correct size to the start and end
+        # of the MFCC feature matrix
+
+        # Pick up to numcontext time slices in the past, and complete with empty
+        # mfcc features
+        need_empty_past = max(0, (context_past_min - time_slice))
         empty_source_past = list(empty_mfcc for empty_slots in range(need_empty_past))
         data_source_past = orig_inputs[max(0, time_slice - numcontext):time_slice]
         assert(len(empty_source_past) + len(data_source_past) == numcontext)
 
+        # Pick up to numcontext time slices in the future, and complete with empty
+        # mfcc features
+        need_empty_future = max(0, (time_slice - context_future_max))
+        empty_source_future = list(empty_mfcc for empty_slots in range(need_empty_future))
+        data_source_future = orig_inputs[time_slice + 1:time_slice + numcontext + 1]
+        assert(len(empty_source_future) + len(data_source_future) == numcontext)
 
-readWav("../data/catorce_0_2.wav" , 13)
+        if need_empty_past:
+            past = np.concatenate((empty_source_past, data_source_past))
+        else:
+            past = data_source_past
+
+        if need_empty_future:
+            future = np.concatenate((data_source_future, empty_source_future))
+        else:
+            future = data_source_future
+
+        past = np.reshape(past, numcontext * numcep)
+        now = orig_inputs[time_slice]
+        future = np.reshape(future, numcontext * numcep)
+
+        train_inputs[time_slice] = np.concatenate((past, now, future))
+        assert(len(train_inputs[time_slice]) == numcep + 2 * numcep * numcontext)
+
+    # Scale/standardize the inputs
+    # This can be done more efficiently in the TensorFlow graph
+    train_inputs = (train_inputs - np.mean(train_inputs)) / np.std(train_inputs)
+    return train_inputs
+
+print(input_vector("../data/catorce_0_2.wav" , 13, 9))
