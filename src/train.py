171a9f56ca5d61d0c0f7a53becde73d36df50515
ferllini13 <ferllini_13@hotmail.com>
2018-11-19 15:13:48 -0600

    code refactor

--- src/train.py
+++ src/train.py
@@ -7,19 +7,27 @@ from keras.layers import LSTM
 from main import get_training_set
 
 
+def train():
+    x_train,y_train=get_training_set()
+    x_test=x_train[1000:]
+    y_test=y_train[1000:]
+    x_train=x_train[:1000]
+    y_train=y_train[:1000]
+    print(x_train.shape())
 
-x_train,y_train=get_training_set()
-x_test=x_train[1000:]
-y_test=y_train[1000:]
-x_train=x_train[:1000]
-y_train=y_train[:1000]
-print(x_train.shape())
+
+    model = Sequential()
+    model.add(LSTM(16, input_shape,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(Dense(16, activation='softmax'))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    print(model.summary())
 
 
-model = Sequential()
-model.add(LSTM(16, input_shape,return_sequences=True))
-model.add(Dropout(0.3))
-model.add(Dense(16, activation='softmax'))
-model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+
+if __name__=='__main__':
+    train():
+
+
