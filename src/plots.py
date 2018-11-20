41c9db3a53871c7ad9638b41969d7d681404841a
ferllini13 <ferllini_13@hotmail.com>
2018-11-19 19:07:26 -0600

    plot n refactor

---
+++ src/plots.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import sys
+
+
+def plot_confusion_matrix(cm, classes,title):
+
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], '.2f'),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+
+
